ba79ed399db690b99d497edb0a5ed0a6c0f39e4c
lbarsidati22 <lbarsidati575@gmail.com>
2025-12-13 04:02:53 +0000

    home intent with mvi

--- lib/features/home/presentation/cubit/home_state.dart
+++ lib/features/home/presentation/cubit/home_state.dart
@@ -1,10 +1,50 @@
-part of 'home_cubit.dart';
+import 'package:Ecommerce/features/home/domain/entities/brand_entity.dart';
+import 'package:Ecommerce/features/home/domain/entities/category_entity.dart';
+import 'package:Ecommerce/features/home/domain/entities/product_entity.dart';
 
-abstract class HomeState extends Equatable {
-  const HomeState();
-
-  @override
-  List<Object> get props => [];
-}
+sealed class HomeState {}
 
 class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeSuccess extends HomeState {
+  final List<CategoryEntity>? categories;
+  final List<ProductEntity>? products;
+  final List<BrandEntity>? brands;
+  final String? selectedBrandId;
+  final bool isProductsLoading;
+
+  HomeSuccess({
+    this.categories,
+    this.products,
+    this.brands,
+    this.selectedBrandId,
+    this.isProductsLoading = false,
+  });
+
+  HomeSuccess copyWith({
+    List<CategoryEntity>? categories,
+    List<ProductEntity>? products,
+    List<BrandEntity>? brands,
+    String? selectedBrandId,
+    bool isSelectedBrandIdNull = false, // Flag to explicitly set null
+    bool? isProductsLoading,
+  }) {
+    return HomeSuccess(
+      categories: categories ?? this.categories,
+      products: products ?? this.products,
+      brands: brands ?? this.brands,
+      selectedBrandId: isSelectedBrandIdNull
+          ? null
+          : (selectedBrandId ?? this.selectedBrandId),
+      isProductsLoading: isProductsLoading ?? this.isProductsLoading,
+    );
+  }
+}
+
+class HomeError extends HomeState {
+  final String message;
+
+  HomeError(this.message);
+}
