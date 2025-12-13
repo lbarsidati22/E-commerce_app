45e34d0162197005294df3b1e398e06029a4a129
lbarsidati22 <lbarsidati575@gmail.com>
2025-12-13 01:28:52 +0000

    api client

--- lib/core/api_layer/api_client/api_client.dart
+++ lib/core/api_layer/api_client/api_client.dart
@@ -2,6 +2,8 @@ import 'package:Ecommerce/features/auth/data/models/request/sign_in_request.dart
 import 'package:Ecommerce/features/auth/data/models/request/sign_up_request.dart';
 import 'package:Ecommerce/features/auth/data/models/response/sign_in_response.dart';
 import 'package:Ecommerce/features/auth/data/models/response/sign_up_response.dart';
+import 'package:Ecommerce/features/home/data/models/category_response_model.dart';
+import 'package:Ecommerce/features/home/data/models/products_response_model.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
@@ -22,4 +24,11 @@ abstract class ApiClient {
 
   @POST(Endpoints.signIn)
   Future<SignInResponse> signIn(@Body() SignInRequestBody request);
+
+  //HOME
+  @GET(Endpoints.getAllProducts)
+  Future<ProductsResponseModel> getAllProducts();
+
+  @GET(Endpoints.getAllCategories)
+  Future<CategoryResponseModel> getAllCategories();
 }
--- lib/core/api_layer/api_client/api_client.g.dart
+++ lib/core/api_layer/api_client/api_client.g.dart
@@ -77,6 +77,60 @@ class _ApiClient implements ApiClient {
     return _value;
   }
 
+  @override
+  Future<ProductsResponseModel> getAllProducts() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ProductsResponseModel>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/products',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ProductsResponseModel _value;
+    try {
+      _value = ProductsResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<CategoryResponseModel> getAllCategories() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<CategoryResponseModel>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/categories',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late CategoryResponseModel _value;
+    try {
+      _value = CategoryResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, _result);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
